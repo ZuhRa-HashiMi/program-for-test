0be984e60a96846cf597ed6c9fd5cdd808e60dce
ZuhRa HashiMi <74463413+ZuhRa-HashiMi@users.noreply.github.com>
2022-01-28 12:48:29 +0430

    dic and set

--- 1 pro.py
+++ 1 pro.py
@@ -1,26 +1,11 @@
-class Pit:
-    def __init__(self,name, animal_type, age):
-        self.__name=name
-        self.__animal_type=animal_type
-        self.__age=age
-    def set_name(self,name):
-        self.__name=name
-    def set_animal_type(self,animal_type):
-        self.__animal_type=animal_type
-    def age(self,age):
-        self.__age=age
-    def get_name(self):
-        return self.__name
-    def get_animal_type(self):
-        return self.__animal_type
-    def get_age(self):
-        return self.__age
-name=input('\nWhat is youe pit name?')
-animal_type=input('\nWhat is the type of your pit?')
-age=int(input('enter the age of your piy?'))
-pit1= Pit(name,animal_type,age)
-print('\nName',pit1.get_name())
-print('\nAnimal type', pit1.get_animal_type())
-print('\nAge', pit1.get_age())
-#because of learning, I add comment
+gal_moon={'io':'182.21', 'Euop':'1560.8'}
+moon_surface={'io':'1.796', 'Euop':'1.314'}
+moon_orbital={'io':'1.769', 'Euop':'3.551'}
+print('which property do you want:')
+for moon in gal_moon:
+    print(moon)
+property=input(':')
 
+print(gal_moon.get(property))
+print(moon_surface.get(property))
+print(moon_orbital.get(property))
